d573d3dfb6aebb26223e80b5c49c680fa0a17665
crist <crist@DESKTOP-160NDDP>
2020-11-12 17:06:15 +0200

    Initial commit - add base Spring Boot project

---
+++ accountmgr/src/main/java/com/assignment/accountmgr/AccountmgrApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.accountmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmgrApplication.class, args);
+	}
+
+}
---
+++ accountmgr/src/test/java/com/assignment/accountmgr/AccountmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.accountmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
